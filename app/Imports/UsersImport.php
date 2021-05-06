fbee878fa8d39d6320bdc255fb50d4b37cb435b5
aidieldaniel <aidiel.works@gmail.com>
2021-05-06 23:12:02 +0800

    Import Excel File Function
    Import excel file to create, update and delete.

---
+++ app/Imports/UsersImport.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\SkipsErrors;
+use Maatwebsite\Excel\Concerns\SkipsFailures;
+use Maatwebsite\Excel\Concerns\SkipsOnError;
+use Maatwebsite\Excel\Concerns\SkipsOnFailure;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
+use Maatwebsite\Excel\Validators\Failure;
+use Throwable;
+
+class UsersImport implements
+    ToModel,
+    WithHeadingRow,
+    SkipsOnError,
+    WithValidation,
+    SkipsOnFailure
+{
+
+    use Importable, SkipsErrors, SkipsFailures;
+
+    /**
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
+    public function model(array $row)
+    {
+        return new User([
+            'name' => $row['name'],
+            'email' => $row['email'],
+            'password' => Hash::make('password')
+        ]);
+    }
+
+    public function rules(): array
+    {
+        return [
+            '*.email' => ['unique:users', 'email']
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,14 @@
 
 use App\Http\Resources\UserCollection;
 use App\Http\Resources\UserResource;
+use App\Imports\UsersImport;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Maatwebsite\Excel\Facades\Excel;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +26,11 @@ use Illuminate\Support\Facades\Auth;
 //     return request()user();
 // });
 
-//Show All User
+
+
+
+
+//Login to get token 
 Route::post('/login', function () {
     $login = request()->validate([
         'email' => 'required|string',
@@ -37,6 +45,7 @@ Route::post('/login', function () {
 
     return response(['user' => Auth::user(), compact('access_token')]);
 });
+
 
 Route::middleware('auth:api')->group(function () {
 
@@ -56,6 +65,8 @@ Route::middleware('auth:api')->group(function () {
         return new UserCollection($users);
     });
 
+
+
     //Create user
     Route::post('/user', function () {
         request()->validate([
@@ -71,11 +82,15 @@ Route::middleware('auth:api')->group(function () {
         ]);
     });
 
+
+
     //Show specific user
     Route::get('/user/{user}', function (User $user) {
 
         return new UserResource($user);
     });
+
+
 
     //Update User
     Route::patch('/user/{user}', function (User $user) {
@@ -96,6 +111,8 @@ Route::middleware('auth:api')->group(function () {
         ];
     });
 
+
+
     //Delete User
     Route::delete('/user/{user}', function (User $user) {
         $success = $user->delete();
@@ -103,5 +120,108 @@ Route::middleware('auth:api')->group(function () {
         return [
             'success' => $success
         ];
+    });
+
+
+
+
+
+    //Create user via excel file
+    Route::post('/user/excel/create', function (Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'file' => 'mimes:xlsx,xls,csv',
+        ]);
+
+        if ($validator->fails()) {
+            return [
+                'message' => 'This is not excel file',
+            ];
+        }
+
+        $file = $request->file('file');
+
+        $import = new UsersImport;
+
+        $import->import($file);
+
+        if ($import->errors() != null && $import->failures() != null) {
+            return [
+                'message' => 'Certain file not insert into database',
+                'error' => $import->errors(),
+                'failure' => $import->failures()
+            ];
+        } else {
+            return [
+                'message' => 'File uploaded. User created in database',
+            ];
+        }
+    });
+
+
+
+    //update user via excel file
+    Route::post('/user/excel/update', function (Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'file' => 'mimes:xlsx,xls,csv',
+        ]);
+
+        if ($validator->fails()) {
+            return [
+                'message' => 'This is not excel file',
+            ];
+        }
+
+        $file = $request->file('file');
+
+        $users = Excel::toCollection(new UsersImport, $file);
+
+        foreach ($users[0] as $user) {
+
+            if (User::where('email', $user['email'])->exists()) {
+
+                $dbuser = User::where('email', $user['email'])->get();
+
+                if ($user['name'] != $dbuser[0]['name']) {
+
+                    User::where('id',  $dbuser[0]['id'])
+                        ->update([
+                            'name' => $user['name']
+                        ]);
+                }
+            }
+        }
+        return ['message' => 'File uploaded. Database updated.'];
+    });
+
+
+
+    //delete user via excel file
+    Route::post('/user/excel/delete', function (Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'file' => 'mimes:xlsx,xls,csv',
+        ]);
+
+        if ($validator->fails()) {
+            return [
+                'message' => 'This is not excel file',
+            ];
+        }
+
+        $file = $request->file('file');
+
+        $users = Excel::toCollection(new UsersImport, $file);
+
+        foreach ($users[0] as $user) {
+
+            if (User::where('email', $user['email'])->exists()) {
+
+                User::where('email', $user['email'])->delete();
+            }
+        }
+
+        return ['message' => 'File uploaded. Users deleted.'];
     });
 });
